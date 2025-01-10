3bc7653fc58f3b529d6604c09c44b4c45b00c84c
Rahul-Encoded <rahulm03@outlook.com>
2025-01-11 04:24:00 +0530

    Add WhoToFollow component to Home page layout and fix class name for grid column span

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 //By default every component is a server component by default
 
 import CreatePost from "@/components/CreatePost";
+import WhoToFollow from "@/components/WhoToFollow";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
@@ -9,12 +10,12 @@ export default async function Home() {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
-      <div className="lg:colspan-6">
+      <div className="lg:col-span-6">
         {user ? <CreatePost></CreatePost> : null}
       </div>
 
       <div className="hidden lg:block lg:col-span-4 sticky top-20">
-        Who to follow?
+        <WhoToFollow></WhoToFollow>
       </div>
     </div>
   );
